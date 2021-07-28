271d1ef4c295660b4c3c3c98d52fbe45d7817bb6
Chen Qiu <qiu.chen1@northeastern.edu>
2021-07-28 10:40:09 -0700

    update ltcode.253 meetingRoomsII sweep line solution

---
+++ sweep_line/meetingRoomsII.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+// leetcode 253
+public class meetingRoomsII {
+    // sweep line
+    public static int findMinNumRooms(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> (a[0] - b[0]));
+        //int[][] weights = new int[intervals.length*2][2];
+        ArrayList<int[]> weights = new ArrayList<>(intervals.length * 2);
+        for (int[] i : intervals) {
+            weights.add(new int[]{i[0], 1});
+            weights.add(new int[]{i[1], -1});
+        }
+        Collections.sort(weights, (a, b) -> (a[0] - b[0])); // sort by time(start or end)
+        int count = 0;
+        int res = 0;
+        for (int[] i : weights) {
+            if (i[1] == 1) {
+                count++;
+            } else {
+                count--;
+            }
+            res = Math.max(count, res);
+        }
+        return res;
+
+    }
+
+    public static void testCountMin(int[][] intervals) {
+        String strInterval = intervals.toString();
+        System.out.println(strInterval + ": " + findMinNumRooms(intervals));
+    }
+
+    public static void main(String[] args) {
+        int[][] intervals1= {{0, 30}, {5, 10}, {15, 20}};
+        int[][] intervals2= {{7, 10}, {2, 4}};
+        int[][] intervals3= {{1, 10}, {2, 7}, {3, 19}, {8, 12}, {10, 20}, {11, 30}};
+
+        testCountMin(intervals1);
+        testCountMin(intervals2);
+        testCountMin(intervals3);
+
+
+    }
+
+
+}
