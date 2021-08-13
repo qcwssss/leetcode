5b86e5257b6343661803d76a82f1033469b9f9c6
Chen Qiu <qiu.chen1@northeastern.edu>
2021-08-13 09:17:45 -0700

    update my vertical scan answer, it works but very slow

--- Array/src/array_leet/LongestCommonPrefix.java
+++ Array/src/array_leet/LongestCommonPrefix.java
@@ -1,5 +1,6 @@
 package array_leet;
 
+/* Scan all words horizontally.  */
 public class LongestCommonPrefix {
   public String longestCommonPrefix1(String[] strs) {
     String prefix = strs[0];
@@ -13,6 +14,7 @@ public class LongestCommonPrefix {
 
   }
 
+  /* Scan vertically. This method runs really FAST!!! */
   public static String longestCommonPrefix2(String[] strs) {
     if (strs == null || strs.length == 0) return "";
     for (int i = 0; i < strs[0].length() ; i++){
@@ -25,10 +27,27 @@ public class LongestCommonPrefix {
     return strs[0];
   }
 
+  public static String longestCommonPrefixMyAns(String[] strs) {
+    String prefix = strs[0]; // assume prefix is the first word
+    for (int i = 0; i < strs[0].length(); i++) { //string object use .lengh() "method"
+      for (int j = 1; j < strs.length; j++) { // array use .length "variable"
+        if (strs[j].indexOf(prefix) != 0 ) {
+          prefix = prefix.substring(0, prefix.length() - 1);
+          break;
+        }
+        if (prefix.length() == 0) return prefix;
+      }
+    }
+    return prefix;
+  }
+
+
+
   public static void main(String[] args) {
     String[] s1 = {"flower","flow","flight"};
     String[] s0 = {""};
     String[] s2 = {"a", "b"};
+    String[] s3 = {"cat", "dog", "cannon"};
 
     String ans = LongestCommonPrefix.longestCommonPrefix2(s1);
     String ans0 = LongestCommonPrefix.longestCommonPrefix2(s0);
@@ -36,6 +55,9 @@ public class LongestCommonPrefix {
     System.out.println(ans);
     System.out.println("ans0: " + ans0);
     System.out.println("ans02: " + LongestCommonPrefix.longestCommonPrefix2(s2));
+    System.out.println("ans02(official solution): " + LongestCommonPrefix.longestCommonPrefix2(s2));
+    System.out.println("ans02(my own): " + LongestCommonPrefix.longestCommonPrefixMyAns(s2));
+    System.out.println("ans03(my own): " + LongestCommonPrefix.longestCommonPrefixMyAns(s3));
 
 
   }
