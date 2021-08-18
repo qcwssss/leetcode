a0b8c62f6b2cbe55e6b08b7689b752625638424b
Chen Qiu <qiu.chen1@northeastern.edu>
2021-08-18 09:43:58 -0700

    update official solution

--- Array/src/array_leet/RemoveDupFromSorted.java
+++ Array/src/array_leet/RemoveDupFromSorted.java
@@ -3,27 +3,21 @@ package array_leet;
 import org.junit.Test;
 
 public class RemoveDupFromSorted {
+
+  // official solution, using two pointers
   public int removeDuplicates(int[] nums) {
-    int size = nums.length;
-    for (int i = 0; i < size; i++) {
-      if (nums[i] == nums[i + 1]) {
-        nums[i] = Integer.MIN_VALUE;
-        moveFoward(nums, i);
-        size--;
+    if (nums.length == 0) return 0;
+    int i = 0;
+    for (int j = 1; j < nums.length; j++) {
+      if (nums[j] != nums[i]) {
+        i++;
+        nums[i] = nums[j];
       }
     }
-    return size;
+    return i + 1;
   }
 
-  private void moveFoward(int[] nums, int x) {
-    for (int i = x; i < nums.length; i++) {
-      if (i == nums.length - 1) {
-        nums[i] = Integer.MIN_VALUE;
-      } else {
-        nums[x] = nums[x + 1];
-      }
-    }
-  }
+
 
   @Test
   public void testRemoveDupFromSortedArray() {
