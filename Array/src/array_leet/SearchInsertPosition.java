7d4f33fb595860bd707c5207bac41b5f1e54e4ba
Chen Qiu <qiu.chen1@northeastern.edu>
2021-08-24 09:34:46 -0700

    update bi-search, minor mistakes exist

--- Array/src/array_leet/SearchInsertPosition.java
+++ Array/src/array_leet/SearchInsertPosition.java
@@ -5,20 +5,6 @@ import org.junit.Test;
 
 public class SearchInsertPosition {
 
-  public int searchInsert(int[] nums, int target) {
-    // brute force
-    int len = nums.length;
-    int returnVal = 0;
-    if (target < nums[0]) return 0;
-    if (target > nums[len - 1]) return len;
-
-    for (int i = 0; i < len; i++) {
-      if (target == nums[i]) return i;
-      if (target > nums[i] && target < nums[i + 1]) return i + 1;
-    }
-
-    return len;
-  }
 
   /**
    * @source https://github.com/qcwssss/leetcode-master/blob/master/problems/0035.%E6%90%9C%E7%B4%A2%E6%8F%92%E5%85%A5%E4%BD%8D%E7%BD%AE.md
@@ -32,15 +18,43 @@ public class SearchInsertPosition {
     return len;
   }
 
+  public static int search2(int[] nums, int target) {
+    int len = nums.length;
+    int left = 0;
+    int right = len -1;
+
+    while (left < right) {
+      int mid = (right + left) / 2;
+      int midVal = nums[mid];
+
+      if (midVal < target) left = mid + 1; // cuz midVal != target
+      else if (midVal > target) right = mid;
+      else return mid;
+
+    }
+
+    return right;
+  }
+
   @Test
   public void testSeachInsert() {
     int[] nums1 = { 1,3,5,6};
     int t1 = 5;
     Assert.assertEquals(2, search(nums1, t1));
 
-    int[] nums0 = {1,3,5,6};
     int tar0 = 0;
-    int ex0 = 0;
-    Assert.assertEquals(ex0, search(nums0, tar0));
+    Assert.assertEquals(0, search(nums1, tar0));
+
+    int t2 = 2;
+    Assert.assertEquals(1, search2(nums1, t2));
+
+    int t7 = 7;
+    Assert.assertEquals(4, search2(nums1, t7));
+
   }
+
+
+
+
+
 }
