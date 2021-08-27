ac0aacd8ea35f94a559e1e3b2835b2bc1b3a0aca
Chen Qiu <qiu.chen1@northeastern.edu>
2021-08-27 08:39:58 -0700

    update #35. Search Insert Position -- binary search solution

--- Array/src/binarySearch/SearchInsertPosition.java
+++ Array/src/binarySearch/SearchInsertPosition.java
@@ -5,8 +5,8 @@ import org.junit.Test;
 
 public class SearchInsertPosition {
 
-
   /**
+   * #1.Brute force way
    * @source https://github.com/qcwssss/leetcode-master/blob/master/problems/0035.%E6%90%9C%E7%B4%A2%E6%8F%92%E5%85%A5%E4%BD%8D%E7%BD%AE.md
    */
   public static int search(int[] nums, int target) {
@@ -18,19 +18,15 @@ public class SearchInsertPosition {
     return len;
   }
 
+  /** #2. Binary Search */
   public static int search2(int[] nums, int target) {
-    int len = nums.length;
-    int left = 0;
-    int right = len -1;
-
+    int left = 0, right = nums.length;
     while (left < right) {
-      int mid = (right + left) / 2;
+      int mid = left + (right - left) / 2;
       int midVal = nums[mid];
-
-      if (midVal < target) left = mid + 1; // cuz midVal != target
+      if (midVal < target) left = mid + 1; // search the right half
       else if (midVal > target) right = mid;
       else return mid;
-
     }
 
     return right;
@@ -38,7 +34,7 @@ public class SearchInsertPosition {
 
   @Test
   public void testSeachInsert() {
-    int[] nums1 = { 1,3,5,6};
+    int[] nums1 = {1, 3, 5, 6};
     int t1 = 5;
     Assert.assertEquals(2, search(nums1, t1));
 
@@ -50,11 +46,5 @@ public class SearchInsertPosition {
 
     int t7 = 7;
     Assert.assertEquals(4, search2(nums1, t7));
-
   }
-
-
-
-
-
 }
