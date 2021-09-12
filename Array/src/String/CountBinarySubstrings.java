e9cfa9e1c449fd4e5f6afd32f2361ab382423f1f
Chen Qiu <qiu.chen1@northeastern.edu>
2021-09-11 17:31:29 -0700

    update my group solution

--- Array/src/String/CountBinarySubstrings.java
+++ Array/src/String/CountBinarySubstrings.java
@@ -5,63 +5,50 @@ import org.junit.Test;
 
 public class CountBinarySubstrings {
 
+  /** 696. Count Binary Substrings. */
   public int countBinarySubstrings(String s) {
-    // two pointer, brute force
-    int cnt1 = 0, cnt0 = 0, res = 0; // cnt1 is count the char of head
-    int n = s.length();
-
-    int j = 0;
-    while (j < n) {
-      char head = s.charAt(j);
-      for (int i = j + 1; i < s.length(); i++) {
-        if (cnt1 == cnt0 && cnt1 != 0) {
-          res++;
-          break;
-        }
-
-        if (s.charAt(i) != head) {
-          cnt0++;
-          continue;
-        }
-        if (s.charAt(i) == head) {
-          if (cnt0 == 0) {
-            cnt1++;
-          } else {
-            break;
-          }
-        }
+    // write by myself
+    int cur = 1; // current number of 1/0
+    int prev = 0; // number of previous digit
+    int count = 0;
+    char[] arr = s.toCharArray();
+    for (int i = 1; i < s.length(); i++) {
+      if (arr[i] != arr[i - 1]) {
+        count += Math.min(prev, cur);
+        prev = cur;
+        cur = 1;
+      } else {
+        cur++;
       }
-      j++;
     }
-
-    return res;
+    count += Math.min(prev, cur); // add the last group
+    return count;
   }
 
   /**
    * @source https://www.youtube.com/watch?v=MGPHPadxhtQ
+   * @source
+   *     https://leetcode.com/problems/count-binary-substrings/discuss/108625/JavaC%2B%2BPython-Easy-and-Concise-with-Explanation
    */
   public int countBinarySubstrings_Group(String s) {
     int count = 0, i = 1, prev = 0, curr = 1;
-    while(i < s.length()) {
-      //11000110
-      if(s.charAt(i-1) != s.charAt(i)) {
-        count+=Math.min(prev, curr);
+    while (i < s.length()) { // 11000110
+      if (s.charAt(i - 1) != s.charAt(i)) {
+        count += Math.min(prev, curr);
         prev = curr;
         curr = 1;
-      }
-      else {
+      } else {
         curr++;
       }
       i++;
     }
 
-    return count+=Math.min(prev, curr);
+    return count += Math.min(prev, curr);
   }
 
   @Test
   public void testCountBinarySubStrs() {
     String s1 = "00110011";
-    Assert.assertEquals(6, countBinarySubstrings_Group(s1));
-
+    Assert.assertEquals(6, countBinarySubstrings(s1));
   }
 }
