91fafab2f0a597bc40afb63509093e1d46238439
Chen Qiu <qiu.chen1@northeastern.edu>
2021-09-13 09:36:15 -0700

    update #69. addBinaryString

--- Array/src/String/AddBinary.java
+++ Array/src/String/AddBinary.java
@@ -13,7 +13,7 @@ public class AddBinary {
     for (int i = 0, j = 0; i <= n1 || j <= n2; i++, j++) {
       int sum = carry;
       int x = n1 - i >= 0 ? a.charAt(n1 - i) - '0' : 0;
-      int y = n2 - j >= 0 ? a.charAt(n2 - j) - '0' : 0;
+      int y = n2 - j >= 0 ? b.charAt(n2 - j) - '0' : 0;
       sum += x + y;
       carry = sum / 2;
       sb.append(sum % 2);
@@ -28,6 +28,13 @@ public class AddBinary {
   public void testAddBinary() {
     String num1 = "11", num2 = "1";
     Assert.assertEquals("100", addBinary(num1, num2));
+  }
+
+
+  @Test
+  public void testAddBinary2() {
+    String num1 = "1010", num2 = "1011";
+    Assert.assertEquals("10101", addBinary(num1, num2));
   }
 
 }
