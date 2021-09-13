d7b1b8bd10cfd4470e5925d1626561f514f9fd4a
Chen Qiu <qiu.chen1@northeastern.edu>
2021-09-13 09:21:30 -0700

    update #69. addBinaryString

--- Array/src/String/AddBinary.java
+++ Array/src/String/AddBinary.java
@@ -6,22 +6,21 @@ import org.junit.Test;
 public class AddBinary {
 
   public String addBinary(String a, String b) {
-    StringBuilder sum = new StringBuilder();
-    int n1 = a.length(), n2 = b.length();
-    for (int i = 0, j = 0; i < n1 || j < n2; i++, j++) {
-      int carry = 0;
+    StringBuilder sb = new StringBuilder();
+    int n1 = a.length() - 1, n2 = b.length() - 1;
+    int carry = 0;
 
-      int x = i < n1 ? a.charAt(n1 - 1 - i) - '0' : 0;
-      int y = j < n2 ? a.charAt(n2 - 1 - j) - '0' : 0;
-      if (x + y > 1) {
-        carry = 1;
-        sum.append(0);
-      } else {
-        sum.append(x + y);
-      }
-      sum.append(carry);
+    for (int i = 0, j = 0; i <= n1 || j <= n2; i++, j++) {
+      int sum = carry;
+      int x = n1 - i >= 0 ? a.charAt(n1 - i) - '0' : 0;
+      int y = n2 - j >= 0 ? a.charAt(n2 - j) - '0' : 0;
+      sum += x + y;
+      carry = sum / 2;
+      sb.append(sum % 2);
+
     }
-    return sum.toString();
+    if (carry != 0) sb.append(carry);
+    return sb.reverse().toString();
 
   }
 
