740bd4487f60cae40a367a4ffefb74e35776f4fd
Chen Qiu <qiu.chen1@northeastern.edu>
2021-09-16 09:53:07 -0700

    update O(1) space solution

--- Array/src/LinkedList/PalindromeLinkedList.java
+++ Array/src/LinkedList/PalindromeLinkedList.java
@@ -2,6 +2,7 @@ package LinkedList;
 
 import java.util.ArrayList;
 
+/** #234. Palindrome Linked List */
 public class PalindromeLinkedList {
 
   public class ListNode {
@@ -20,14 +21,13 @@ public class PalindromeLinkedList {
     }
   }
 
-  /**  Approach #1: Convert into an array. */
+  /** Approach #1: Convert into an array. */
   public boolean isPalindrome(ListNode head) {
     ListNode ptr = head;
     ArrayList<Integer> aList = new ArrayList<>();
     while (ptr != null) {
       aList.add(ptr.val);
       ptr = ptr.next;
-
     }
     int i = 0, j = aList.size() - 1;
     while (i <= j) {
@@ -41,25 +41,42 @@ public class PalindromeLinkedList {
   }
 
   /**
-   * Approach #2:  reversing the 2nd half and compare the two halves.
+   * Approach #2: reversing the 2nd half and compare the two halves.
+   *
+   * @source
+   *     https://leetcode.com/problems/palindrome-linked-list/discuss/64501/Java-easy-to-understand
    */
   public boolean isPalindrome2(ListNode head) {
-    ListNode ptr = head;
-    ArrayList<Integer> aList = new ArrayList<>();
-    while (ptr != null) {
-      aList.add(ptr.val);
-      ptr = ptr.next;
-
+    // find middle
+    ListNode slow = head, fast = head;
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
     }
-    int i = 0, j = aList.size() - 1;
-    while (i <= j) {
-      if (aList.get(i) != aList.get(j)) {
+
+    // reverse second half
+    ListNode head1 = head;
+    ListNode head2 = reverse(slow);
+    // check palindrome
+    while (head1 != null && head2 != null) {
+      if (head1.val != head2.val) {
         return false;
       }
-      i++;
-      j--;
+      head1 = head1.next;
+      head2 = head2.next;
     }
+
     return true;
   }
 
+  private ListNode reverse(ListNode n) {
+    ListNode newHead = null;
+    while (n != null) {
+      ListNode next = n.next;
+      n.next = newHead;
+      newHead = n;
+      n = next;
+    }
+    return newHead;
+  }
 }
