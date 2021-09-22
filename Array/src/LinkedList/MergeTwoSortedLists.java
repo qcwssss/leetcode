c92cacedc422df17b341070e2fc51fa78e49a7ed
Chen Qiu <qiu.chen1@northeastern.edu>
2021-09-21 20:25:41 -0700

    update recursive solution

--- Array/src/LinkedList/MergeTwoSortedLists.java
+++ Array/src/LinkedList/MergeTwoSortedLists.java
@@ -1,5 +1,8 @@
 package LinkedList;
 
+import java.util.List;
+
+// #21.
 public class MergeTwoSortedLists {
 
   /* iterative  */
@@ -23,15 +26,31 @@ public class MergeTwoSortedLists {
 
   }
 
-  public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-    if (l1 == null) return l1;
-    if (l2 == null) return l2;
+  public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    // link un-linked nodes, when one LList is exhausted
+    if (l1 == null) return l2;
+    if (l2 == null) return l1;
 
     if (l1.val < l2.val) {
-      return mergeTwoLists(l1.next, l2);
+      l1.next = mergeTwoLists(l1.next, l2);
+      return l1;
     } else {
-      return mergeTwoLists(l2.next, l1);
+      l2.next = mergeTwoLists(l2.next, l1);
+      return l2;
     }
+  }
+
+  public static void main(String[] args) {
+    ListNode l1 = new ListNode(1);
+    l1.next = new ListNode(2);
+    l1.next.next = new ListNode(4);
+
+    ListNode l2 = new ListNode(1);
+    l2.next = new ListNode(3);
+    l2.next.next = new ListNode(5);
+
+    ListNode merged = mergeTwoLists(l1, l2);
+    System.out.println(merged);
   }
 
   }
