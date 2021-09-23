e5fb1365babc9e32a3f7d1b915f5f8a42ca276a1
Chen Qiu <qiu.chen1@northeastern.edu>
2021-09-23 10:09:42 -0700

    update PQ solution

--- Array/src/LinkedList/MergeKSortedLists.java
+++ Array/src/LinkedList/MergeKSortedLists.java
@@ -1,10 +1,13 @@
 package LinkedList;
 
+import org.junit.Test;
+
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class MergeKSortedLists {
 
-  public ListNode mergeKLists(ListNode[] lists) {
+  public ListNode mergeKLists1(ListNode[] lists) {
     int len = lists.length;
     if (len == 0) return lists[0];
 
@@ -27,8 +30,47 @@ public class MergeKSortedLists {
       curr.next = new ListNode(pq.poll());
       curr = curr.next;
     }
-    
+
     return dummy.next;
 
   }
-}
+
+  public ListNode mergeKLists(ListNode[] lists) {
+    PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b) -> a.val - b.val); // ascending order
+    ListNode dummy = new ListNode(0);
+    for (int i = 0; i < lists.length; i++ ) {
+      if (lists[i] != null) pq.offer(lists[i]);
+    }
+      // add new nodes to dummy in ascending order
+      ListNode cur = dummy;
+      while (!pq.isEmpty()) {
+        cur.next = pq.poll();
+        cur = cur.next;
+        if (cur.next != null) pq.offer(cur.next);
+      }
+    return dummy.next;
+  }
+
+
+
+
+
+  @Test
+  public void testMergeK1() {
+    ListNode l1 = new ListNode(1);
+    l1.next = new ListNode(4);
+    l1.next.next = new ListNode(5);
+
+    ListNode l2 = new ListNode(1);
+    l2.next = new ListNode(3);
+    l2.next.next = new ListNode(4);
+
+
+    ListNode l3 = new ListNode(2);
+    l3.next = new ListNode(6);
+    ListNode[] lists = new ListNode[]{l1, l2, l3};
+    ListNode mergeK = mergeKLists(lists);
+    System.out.println(mergeK);
+  }
+
+  }
