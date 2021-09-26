d8a094feb57c46f3f23c332d84ebcfb4ea0d04e3
Chen Qiu <qiu.chen1@northeastern.edu>
2021-09-25 21:37:24 -0700

    update sort array, set solutions

--- Array/src/LinkedList/FindTheDuplicateNumber.java
+++ Array/src/LinkedList/FindTheDuplicateNumber.java
@@ -1,5 +1,6 @@
 package LinkedList;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
@@ -11,10 +12,20 @@ public class FindTheDuplicateNumber {
   2: sort array
   3:
    */
-  public int findDuplicate(int[] nums) {
+  public int findDuplicate1(int[] nums) {
+    // space O(n), time O(n)
     Set<Integer> seen = new HashSet<>();
     for (int num : nums) {
       if (!seen.add(num)) return num;
+    }
+    return -1;
+  }
+
+  //  2: sort array
+  public int findDuplicate2(int[] nums) {
+    Arrays.sort(nums);
+    for (int i = 1; i < nums.length; i++) {
+      if (nums[i] == nums[i-1]) return nums[i];
     }
     return -1;
   }
