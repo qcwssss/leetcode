524a2eeb774244bc3cf2a547d27715ff0069835f
Chen Qiu <qiu.chen1@northeastern.edu>
2021-09-27 10:59:11 -0700

    update solution

--- Array/src/LinkedList/RemoveLinkedListElements.java
+++ Array/src/LinkedList/RemoveLinkedListElements.java
@@ -1,2 +1,18 @@
-package LinkedList;public class RemoveLinkedListElements {
+package LinkedList;
+
+public class RemoveLinkedListElements {
+
+  public ListNode removeElements(ListNode head, int val) {
+    ListNode dummy = new ListNode(0);
+    dummy.next = head;
+    ListNode cur = dummy;
+    while (cur.next != null) {
+      if (cur.next.val == val) {
+        cur.next = cur.next.next;
+      } else {
+        cur = cur.next;
+      }
+    }
+    return dummy.next;
+  }
 }
