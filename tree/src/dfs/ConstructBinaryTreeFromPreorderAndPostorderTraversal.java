54e7e0415018a8bae8b6e7ea1f3a528728c3d74b
Chen Qiu <qiu.chen1@northeastern.edu>
2021-10-08 09:26:48 -0700

    update solution

--- tree/src/dfs/ConstructBinaryTreeFromPreorderAndPostorderTraversal.java
+++ tree/src/dfs/ConstructBinaryTreeFromPreorderAndPostorderTraversal.java
@@ -4,27 +4,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ConstructBinaryTreeFromPreorderAndPostorderTraversal {
+
   int[] preorder;
   int[] postorder;
-  Map<Integer, Integer> preorderMap = new HashMap<>(); // <val, index>
+  Map<Integer, Integer> postorderMap = new HashMap<>(); // <val, index>
+
 
   public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
     this.preorder = preorder;
     this.postorder = postorder;
     int N = preorder.length;
     for (int i = 0; i < N; i++) {
-      preorderMap.put(preorder[i], i);
+      postorderMap.put(postorder[i], i);
     }
-    return helper(N - 1, 0, N - 1);
+    return helper(0, N - 1, 0, N - 1);
   }
 
-  private TreeNode helper(int postEnd, int preStart, int preEnd) {
-    int val = preorder[preStart];
-    TreeNode root = new TreeNode(val);
-    int rootIdx = preorderMap.get(postorder[postEnd - 1]); // next root
-    int leftSize = rootIdx - (preStart + 1);
-    root.left = helper(postEnd - 1, preStart + 1, rootIdx - 1);
-    root.right = helper(postEnd - 1 - leftSize, rootIdx + 1, preEnd);
+  private TreeNode helper(int preStart, int preEnd, int postStart, int postEnd) {
+    if (postStart > postEnd || preStart > preEnd) return null;
+    // when prestart == N - 1, should return in case of exception
+    if (preStart == preEnd) return new TreeNode(preorder[preStart]);
+
+    TreeNode root = new TreeNode(preorder[preStart]);
+    int rootIdx = postorderMap.get(preorder[preStart + 1]); //next root in postorder[]
+    int leftSize = rootIdx - postStart; // idx [0, 1, 2], size = 2-0 = 2
+    root.left = helper(preStart + 1, preStart + 1 + leftSize, postStart, rootIdx);
+    root.right = helper(preStart + 2 + leftSize, preEnd, rootIdx + 1, postEnd - 1);
     return root;
 
 
