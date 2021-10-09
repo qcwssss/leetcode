f450aa25cf397deef3dde532465456be1cb55d92
Chen Qiu <qiu.chen1@northeastern.edu>
2021-10-09 09:14:02 -0700

    update bfs solution

--- tree/src/dfs/ValidateBinarySearchTree.java
+++ tree/src/dfs/ValidateBinarySearchTree.java
@@ -4,13 +4,13 @@ public class ValidateBinarySearchTree {
 
   // approach #1: pass upper and lower bound
   public boolean isValidBST(TreeNode root) {
-    return validHelper(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    return validHelper(root, null, null);
   }
 
-  private boolean validHelper(TreeNode node, int lower, int upper) {
+  private boolean validHelper(TreeNode node, Integer lower, Integer upper) {
       if (node == null) return true;
-      if (upper != Integer.MAX_VALUE && node.val >= upper) return false;
-      if (lower != Integer.MIN_VALUE && node.val <= lower) return false;
+      if (upper != null && node.val >= upper) return false;
+      if (lower != null && node.val <= lower) return false;
       return validHelper(node.left, lower, node.val) && validHelper(node.right, node.val, upper);
   }
 }
