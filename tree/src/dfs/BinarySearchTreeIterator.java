9b06d20a988976849acf19f0f4e65a948f02cd2b
Chen Qiu <qiu.chen1@northeastern.edu>
2021-10-09 15:47:48 -0700

    update arraylist solution

--- tree/src/dfs/BinarySearchTreeIterator.java
+++ tree/src/dfs/BinarySearchTreeIterator.java
@@ -1,2 +1,39 @@
-package dfs;public class BinarySearchTreeIterator {
+package dfs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinarySearchTreeIterator {
+
+  class BSTIterator {
+    int cur = -1;
+    List<TreeNode> inorder;
+    int capacity;
+
+    public BSTIterator(TreeNode root) {
+      inorder = new ArrayList<>();
+      buildList(root, inorder);
+      capacity = inorder.size();
+    }
+
+    private void buildList(TreeNode node, List<TreeNode> list) {
+      if (node == null) return;
+
+      buildList(node.left, list);
+      list.add(node);
+      buildList(node.right, list);
+
+    }
+
+    public int next() {
+      cur++;
+      return inorder.get(cur).val;
+
+    }
+
+    public boolean hasNext() {
+      return cur < capacity - 1;
+
+    }
+  }
 }
