1c63571957348c1f2812ffce12af67948c46c50e
Chen Qiu <qiu.chen1@northeastern.edu>
2021-10-12 16:57:23 -0700

    udpate recursive solution, need to think about it

--- tree/src/dfs/UniqueBinarySearchTreesII.java
+++ tree/src/dfs/UniqueBinarySearchTreesII.java
@@ -7,24 +7,38 @@ public class UniqueBinarySearchTreesII {
 
   public List<TreeNode> generateTrees(int n) {
     if (n == 0) return new LinkedList<>();
-    return helper(0, n);
+    return helper(1, n); // 1 to n
   }
 
-  private List<TreeNode> helper(int start, int end) {
+  List<TreeNode> helper(int start, int end) {
     List<TreeNode> res = new LinkedList<>();
     if (start > end) res.add(null);
+
+    // let each number be the root node
     for (int i = start; i <= end; i++) {
       List<TreeNode> leftList = helper(start, i - 1);
       List<TreeNode> rightList = helper(i + 1, end);
+      // cartesian product of left and right
       for (TreeNode left : leftList) {
         for (TreeNode right : rightList) {
           TreeNode root = new TreeNode(i);
           root.left = left;
           root.right = right;
           res.add(root);
+
         }
       }
     }
     return res;
+
+  }
+
+  public static void main(String[] args) {
+    UniqueBinarySearchTreesII u2 = new UniqueBinarySearchTreesII();
+    List<TreeNode> ans_3 = u2.generateTrees(3);
+    for (TreeNode n : ans_3) {
+      System.out.println(n);
+
+    }
   }
 }
