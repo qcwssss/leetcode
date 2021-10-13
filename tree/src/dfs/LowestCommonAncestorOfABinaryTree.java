1dae13500cf4289108f879015b531ebab9fa2d0b
Chen Qiu <qiu.chen1@northeastern.edu>
2021-10-13 11:04:03 -0700

    update recursive solution

--- tree/src/dfs/LowestCommonAncestorOfABinaryTree.java
+++ tree/src/dfs/LowestCommonAncestorOfABinaryTree.java
@@ -1,44 +1,90 @@
 package dfs;
 
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
+import java.util.*;
 
 public class LowestCommonAncestorOfABinaryTree {
-
+  // divide and conquer
   public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-    Map<TreeNode, TreeNode> parentMap = new HashMap<>(); // <children, parent>
-    Deque<TreeNode> queue = new LinkedList<>();
+    if (root == null || root == p || root == q) return root;
+    // recursive
+    TreeNode left = lowestCommonAncestor(root.left, p, q);
+    TreeNode right = lowestCommonAncestor(root.right, p, q);
 
-    // BFS
-    queue.push(root);
-    TreeNode cur;
-    while (!queue.isEmpty()) {
-      int sizeQ = queue.size();
-      for (int i = 0; i < sizeQ; i++) {
-        cur = queue.poll();
-        if (cur.left != null) {
-          parentMap.put(cur.left, cur);
-          queue.offer(cur.left);
-        }
-        if (cur.right != null) {
-          parentMap.put(cur.right, cur);
-          queue.offer(cur.right);
-        }
-      }
+    if (left != null && right != null) return root;
+    if (left != null) return left;
+    if (right != null) return right;
+    return null;
+
+  }
+
+  public TreeNode lowestCommonAncestor1(TreeNode root, TreeNode p, TreeNode q) {
+    Map<TreeNode, TreeNode> parentMap = new HashMap<>(); // <children, parent>
+    dfs(parentMap, root, null); // build parent map
+
+    Set<TreeNode> parentSet = new HashSet<>();
+    while (p != null) {
+      TreeNode parentOfP = parentMap.get(p);
+      parentSet.add(parentOfP);
+      p = parentOfP;
     }
 
-    // find parent
-  return findParent(parentMap, p, q);
+    while (q != null) {
+      TreeNode parentOfQ = parentMap.get(q);
+      if (parentSet.contains(parentOfQ)) {
+        return parentOfQ;
+      }
+      q = parentOfQ;
+    }
+    return null;
+  }
+
+  private void dfs(Map<TreeNode, TreeNode> parent, TreeNode cur, TreeNode parentNode) {
+    if (cur == null) return;
+
+    parent.put(cur, parentNode);
+    dfs(parent, cur.left, cur);
+    dfs(parent, cur.right, cur);
 
   }
 
-  private TreeNode findParent(Map<TreeNode, TreeNode> map, TreeNode p, TreeNode q) {
-    if (map.get(p) == q) return q;
-    if (map.get(q) == p) return p;
-    if (map.get(p) == map.get(q)) return map.get(p);
 
-    return findParent(map, map.get(p), map.get(q));
+
+
+
+    // my first attempt
+    public TreeNode lowestCommonAncestor_Naive(TreeNode root, TreeNode p, TreeNode q) {
+      Map<TreeNode, TreeNode> parentMap = new HashMap<>(); // <children, parent>
+      Deque<TreeNode> queue = new LinkedList<>();
+
+      // BFS
+      queue.push(root);
+      TreeNode cur;
+      while (!queue.isEmpty()) {
+        int sizeQ = queue.size();
+        for (int i = 0; i < sizeQ; i++) {
+          cur = queue.poll();
+          if (cur.left != null) {
+            parentMap.put(cur.left, cur);
+            queue.offer(cur.left);
+          }
+          if (cur.right != null) {
+            parentMap.put(cur.right, cur);
+            queue.offer(cur.right);
+          }
+        }
+      }
+
+      // find parent
+      return findParent(parentMap, p, q);
+
+    }
+
+    private TreeNode findParent(Map<TreeNode, TreeNode> map, TreeNode p, TreeNode q) {
+      if (map.get(p) == q) return q;
+      if (map.get(q) == p) return p;
+      if (map.get(p) == map.get(q)) return map.get(p);
+
+      return findParent(map, map.get(p), map.get(q));
+    }
   }
-}
+
