7651a773afb5e45bdfbc3ce4f224fc5800893046
Chen Qiu <qiu.chen1@northeastern.edu>
2021-10-15 09:55:45 -0700

    update commet

--- tree/src/dfs/PathSum.java
+++ tree/src/dfs/PathSum.java
@@ -3,16 +3,12 @@ package dfs;
 public class PathSum {
 
   public boolean hasPathSum(TreeNode root, int targetSum) {
+    // root to leaf path, because of that we can subtract root val form sum.
     if (root == null) return false;
     if (root.left == null && root.right == null && targetSum == root.val) return true;
     return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
 
   }
 
-  public boolean hasPathSum2(TreeNode root, int sum) {
-    if(root == null) return false;
-    if(root.left == null && root.right == null) return sum == root.val;
-    return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
-  }
 
 }
