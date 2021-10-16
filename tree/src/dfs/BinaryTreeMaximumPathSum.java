79932f7513bb12618aa64ab72031783167373c1b
Chen Qiu <qiu.chen1@northeastern.edu>
2021-10-16 11:21:08 -0700

    update recursive solution

--- tree/src/dfs/BinaryTreeMaximumPathSum.java
+++ tree/src/dfs/BinaryTreeMaximumPathSum.java
@@ -4,25 +4,32 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class BinaryTreeMaximumPathSum {
-
+  int curMax = Integer.MIN_VALUE;
   public int maxPathSum(TreeNode root) {
-    return helper(root, 0);
+    helper(root);
+    return curMax;
 
   }
-
-  private int helper(TreeNode root, int curMax) {
-    if (root == null) return curMax;
-    curMax = Math.max(root.val + curMax, curMax);
-    int left = helper(root.left, curMax);
-    int right = helper(root.right, curMax);
-    int lrSum = left + right - root.val;
-    return Math.max(lrSum, Math.max(left, right) );
+  /*
+   information pass,
+     1
+    / \
+   2   3
+   This subtree sum is 1+2+3 = 6, pass 6 to its parent.
+   */
+  private int helper(TreeNode root) {
+    if (root == null) return 0;
+    int left = Math.max(0, helper(root.left));
+    int right = Math.max(0, helper(root.right));
+    // update global variable
+    curMax = Math.max(left + right + root.val, curMax);
+    return Math.max(left, right) + root.val;
 
   }
 
   @Test
   public void testMaxPathSum() {
     TreeNode root = new TreeNode(1, new TreeNode(2), new TreeNode(3));
-    Assert.assertEquals(5, maxPathSum(root));
+    Assert.assertEquals(6, maxPathSum(root));
   }
 }
