1247e5841c4e56658ef992dbd94a197257bd0612
Chen Qiu <qiu.chen1@northeastern.edu>
2021-10-21 11:36:36 -0700

    update hashmap solution

--- tree/src/dfs/FindDuplicateSubtrees.java
+++ tree/src/dfs/FindDuplicateSubtrees.java
@@ -2,24 +2,38 @@ package dfs;
 
 import org.junit.Test;
 
-public class FindDuplicateSubtrees {
+import java.util.*;
 
+public class FindDuplicateSubtrees {
   /**
    * https://leetcode.com/problems/find-duplicate-subtrees/discuss/106011/Java-Concise-Postorder-Traversal-Solution
-   * @param root
-   * @return
    */
-  String preorder(TreeNode root) {
-    if (root == null) return "#";
 
-    String left = preorder(root.left);
-    String sequence = root.val + "," + left + "," + preorder(root.right);
-    return sequence;
+  List<TreeNode> dup = new LinkedList<>();
+  public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+    Map<String, Integer> subMap = new HashMap<>();
+    dfs(root, subMap);
+    return dup;
+  }
+
+
+  String dfs(TreeNode root, Map<String, Integer> subMap) {
+    if (root == null) return "#";
+    // postorder
+    String left = dfs(root.left, subMap);
+    String right = dfs(root.right, subMap);
+    String subtree =  left + "," + right + "," +root.val;
+    subMap.put(subtree, subMap.getOrDefault(subtree, 0) + 1);
+    // in case of duplicates root, add to list only when map == 2.
+    if (subMap.get(subtree) == 2) dup.add(root);
+    return subtree;
   }
 
   @Test
-  public void testPreorderString() {
+  public void testDfsString() {
     TreeNode root = new TreeNode(1, new TreeNode(2), new TreeNode(3));
-    System.out.println(preorder(root));
+    Set<String> subTreeSet = new HashSet<>();
+
+    //System.out.println(dfs(root, subTreeSet));
   }
 }
