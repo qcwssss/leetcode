a31b02fbdfc38a78e08f20fe0885a62a4ac11e26
Chen Qiu <qiu.chen1@northeastern.edu>
2021-10-22 11:08:34 -0700

    update bottm up solution

---
+++ DynamicProgramming/src/SingleArray/HouseRobber.java
@@ -0,0 +1,23 @@
+package SingleArray;
+
+public class HouseRobber {
+
+  public int rob(int[] nums) {
+    int N = nums.length;
+    int maxSoFar, maxEnd;
+    int[] memo = new int[N + 1];
+    memo[1] = nums[0];
+    if (N < 2) return memo[1];
+
+    memo[2] = Math.max(nums[1], nums[0]);
+    maxSoFar = Math.max(memo[1], memo[2]);
+    for(int i = 3; i <= N; i++) {
+      memo[i] = Math.max(nums[i-1] + memo[i-2], memo[i-1]);
+      maxSoFar = Math.max(maxSoFar, memo[i]);
+    }
+
+
+    return maxSoFar;
+
+  }
+}
