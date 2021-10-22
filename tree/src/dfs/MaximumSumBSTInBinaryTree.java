4b564d267be5017bef2aa22a29dc23f858d96a91
Chen Qiu <qiu.chen1@northeastern.edu>
2021-10-22 09:36:47 -0700

    passed

--- tree/src/dfs/MaximumSumBSTInBinaryTree.java
+++ tree/src/dfs/MaximumSumBSTInBinaryTree.java
@@ -16,7 +16,7 @@ public class MaximumSumBSTInBinaryTree {
   private int[] dfs(TreeNode root) {
     if (root == null) {
       return new int[]
-              {1, Integer.MIN_VALUE, Integer.MIN_VALUE, 0};
+              {1, Integer.MAX_VALUE, Integer.MIN_VALUE, 0};
       // [isBST(1 = true), min_root, max_root, rootSum]
     }
 
@@ -35,6 +35,43 @@ public class MaximumSumBSTInBinaryTree {
     } else {
       res[0] = 0; // not BST
     }
+    return res;
+  }
+
+
+  int[] traverse(TreeNode root) {
+    // base case
+    if (root == null) {
+      return new int[]
+              {1, Integer.MAX_VALUE, Integer.MIN_VALUE, 0};
+    }
+
+    // 递归计算左右子树
+    int[] left = traverse(root.left);
+    int[] right = traverse(root.right);
+
+    /******* 后序遍历位置 *******/
+    int[] res = new int[4];
+    // 这个 if 在判断以 root 为根的二叉树是不是 BST
+    if (left[0] == 1 && right[0] == 1 &&
+            root.val > left[2] && root.val < right[1]) {
+      // 以 root 为根的二叉树是 BST
+      res[0] = 1;
+      // 计算以 root 为根的这棵 BST 的最小值
+      res[1] = Math.min(left[1], root.val);
+      // 计算以 root 为根的这棵 BST 的最大值
+      res[2] = Math.max(right[2], root.val);
+      // 计算以 root 为根的这棵 BST 所有节点之和
+      res[3] = left[3] + right[3] + root.val;
+      // 更新全局变量
+      maxSum = Math.max(maxSum, res[3]);
+    } else {
+      // 以 root 为根的二叉树不是 BST
+      res[0] = 0;
+      // 其他的值都没必要计算了，因为用不到
+    }
+    /**************************/
+
     return res;
   }
 
