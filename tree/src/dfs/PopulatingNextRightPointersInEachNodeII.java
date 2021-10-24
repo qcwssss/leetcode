3875299701e33f6e0ab8605e338f2c9f55b92384
Chen Qiu <qiu.chen1@northeastern.edu>
2021-10-24 11:35:44 -0700

    update O(1) space solution

---
+++ tree/src/dfs/PopulatingNextRightPointersInEachNodeII.java
@@ -0,0 +1,52 @@
+package dfs;
+
+public class PopulatingNextRightPointersInEachNodeII {
+  class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {
+    }
+
+    public Node(int _val) {
+      val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+      val = _val;
+      left = _left;
+      right = _right;
+      next = _next;
+    }
+  }
+
+  ;
+
+  public Node connect(Node root) {
+    if (root == null) return null;
+
+    Node head = root;
+    while (head != null) {
+      Node dummy = new Node(0); // dummy points to the head of each level
+      Node tmp = dummy; // tmp traverse each level from left to right;
+
+      // build linked list
+      while (head != null) {
+        if (head.left != null) {
+          tmp.next = head.left;
+          tmp = tmp.next;
+        }
+        if (head.right != null) {
+          tmp.next = head.right; // connect
+          tmp = tmp.next;
+        }
+        head = head.next; // go right
+      }
+      head = dummy.next;
+    }
+    return root;
+  }
+
+}
