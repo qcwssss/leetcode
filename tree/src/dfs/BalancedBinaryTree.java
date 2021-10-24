a0ae8c77631d9d0d1fb445f6ff651eeef9d031b8
Chen Qiu <qiu.chen1@northeastern.edu>
2021-10-24 16:16:21 -0700

    udpate O(1) time solution

---
+++ tree/src/dfs/BalancedBinaryTree.java
@@ -0,0 +1,42 @@
+package dfs;
+
+public class BalancedBinaryTree {
+  // O(n^2) time
+  public boolean isBalanced1(TreeNode root) {
+    if (root == null) return true;
+    int left = depth(root.left);
+    int right = depth(root.right);
+    if (Math.abs(left - right) > 1) return false;
+    return isBalanced(root.left) && isBalanced(root.right);
+
+  }
+
+  private int depth(TreeNode root) {
+    if (root == null) return 0;
+    int left = depth(root.left);
+    int right = depth(root.right);
+    return Math.max(left, right) + 1;
+
+  }
+
+  // O(n) time
+  public boolean isBalanced(TreeNode root) {
+    if (root == null) return true;
+    int diff = getDepth(root);
+    return diff != -1;
+
+  }
+  // count depth
+  private int getDepth(TreeNode root) {
+    if (root == null) return 0;
+    int left = getDepth(root.left);
+    int right = getDepth(root.right);
+    // check left right difference
+    int diff = Math.abs(left - right);
+    if (diff > 1 || left == -1 || right == -1) {
+      return -1;
+    }
+    return Math.max(left, right) + 1;
+
+  }
+}
