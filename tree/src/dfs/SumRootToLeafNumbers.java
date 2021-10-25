3ad01d4e9053749388e2188422ed80638e4663c8
Chen Qiu <qiu.chen1@northeastern.edu>
2021-10-24 18:10:19 -0700

    update sumRootToLeaf test

--- tree/src/dfs/SumRootToLeafNumbers.java
+++ tree/src/dfs/SumRootToLeafNumbers.java
@@ -1,5 +1,8 @@
 package dfs;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SumRootToLeafNumbers {
 
   public int sumNumbers(TreeNode root) {
@@ -14,10 +17,17 @@ public class SumRootToLeafNumbers {
     String left = helper(root.left);
     String right = helper(root.right);
 
-    String sumL = String.valueOf(root.val) + String.valueOf(left);
-    String sumR = String.valueOf(root.val) + String.valueOf(right);
+    String sumL = String.valueOf(left);
+    String sumR = String.valueOf(right);
     int max = Integer.valueOf(sumL) + Integer.valueOf(sumR);
     return String.valueOf(max);
 
+  }
+
+  @Test
+  public void testSum() {
+    //    1, 2, 3
+    TreeNode root = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+    Assert.assertEquals(25, sumNumbers(root));
   }
 }
