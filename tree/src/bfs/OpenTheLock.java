b40a1ab4629f9ab8883fd808c9c1331596702b64
Chen Qiu <qiu.chen1@northeastern.edu>
2021-11-06 11:24:33 -0700

    rewrite on my own

--- tree/src/bfs/OpenTheLock.java
+++ tree/src/bfs/OpenTheLock.java
@@ -1,12 +1,14 @@
 package bfs;
 
+import org.junit.Test;
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class OpenTheLock {
 
-  public int openLock(String[] deadends, String target) {
+  public int openLock2(String[] deadends, String target) {
     Set<String> begin = new HashSet<>(), end = new HashSet<>();
     Set<String> deads = new HashSet<>(Arrays.asList(deadends));
     begin.add("0000");
@@ -23,8 +25,8 @@ public class OpenTheLock {
         StringBuilder sb = new StringBuilder(s);
         for (int i = 0; i < s.length(); i++) {
           char c = sb.charAt(i);
-          String s1 = sb.substring(0, i) + (c == '9' ? 0 : c - '0' + 1) + sb.substring(i+1);
-          String s2 = sb.substring(0, i) + (c == '0' ? 9 : c - '0' - 1) + sb.substring(i+1);
+          String s1 = sb.substring(0, i) + (c == '9' ? 0 : c - '0' + 1) + sb.substring(i + 1);
+          String s2 = sb.substring(0, i) + (c == '0' ? 9 : c - '0' - 1) + sb.substring(i + 1);
           if (!deads.contains(s1)) temp.add(s1);
           if (!deads.contains(s2)) temp.add(s2);
 
@@ -40,4 +42,49 @@ public class OpenTheLock {
 
   }
 
+  public int openLock(String[] deadends, String target) {
+    Set<String> deads = new HashSet<>(Arrays.asList(deadends));
+    Set<String> begin = new HashSet<>(), end = new HashSet<>();
+    begin.add("0000");
+    end.add(target);
+
+    // BFS
+    int level = 0;
+    while (!begin.isEmpty() && !end.isEmpty()) {
+      Set<String> temp = new HashSet<>();
+      for (String s : begin) {
+        if (end.contains(s)) return level;
+        if (deads.contains(s)) continue;
+        deads.add(s); // visited
+
+        StringBuilder sb = new StringBuilder(s);
+        for (int i = 0; i < 4; i++) {
+          char c = sb.charAt(i);
+          int c1 = c == '9' ? 0 : c - '0' + 1; // +1
+                // c == '9' ? 0 : c - '0' + 1
+          int c2 = c == '0' ? 9 : c - '0' - 1; // -1
+              //   c == '0' ? 9 : c - '0' - 1
+          String s1 = sb.substring(0, i) + c1 + sb.substring(i+1);
+          String s2 = sb.substring(0, i) + c2 + sb.substring(i+1);
+
+          if (!deads.contains(s1)) temp.add(s1);
+          if (!deads.contains(s2)) temp.add(s2);
+        }
+      }
+      level += 1;
+      begin = end;
+      end = temp;
+
+    }
+
+    return -1;
   }
+
+  @Test
+  public void testLock() {
+    String t1 = "0009";
+    String[] d1 = new String[] {"8888"};
+    openLock(d1, t1);
+  }
+
+}
