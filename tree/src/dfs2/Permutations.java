e7a5ddfd39c77b2a90b6a2ff2caee8a3cea97945
Chen Qiu <qiu.chen1@northeastern.edu>
2021-11-09 19:27:24 -0800

    update brute force solution

---
+++ tree/src/dfs2/Permutations.java
@@ -0,0 +1,38 @@
+package dfs2;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Permutations {
+
+  public List<List<Integer>> permute(int[] nums) {
+    List<List<Integer>> res = new LinkedList<>();
+    //Arrays.sort(nums);
+    List<Integer> temp = new LinkedList<>();
+    helper(res, nums, temp);
+    return res;
+
+  }
+
+  void helper(List<List<Integer>> res, int[] nums, List<Integer> temp) {
+    if (temp.size() == nums.length) {
+      res.add(new LinkedList<>(temp));
+      //return;
+    } else {
+      for (int i = 0; i < nums.length; i++) {
+        if (temp.contains(nums[i])) continue;
+        temp.add(nums[i]);
+        helper(res, nums, temp);
+        temp.remove(temp.size() - 1);
+      }
+    }
+  }
+
+  @Test
+  public void testPermutation() {
+    int[] nums = new int[]{1, 2, 3};
+    System.out.println(permute(nums));
+  }
+}
