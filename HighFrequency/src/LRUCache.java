167ec8ec5701c3bed6eca1994cd2374e5395d680
Chen Qiu <qiu.chen1@northeastern.edu>
2021-11-14 11:07:04 -0800

    update solution

--- HighFrequency/src/LRUCache.java
+++ HighFrequency/src/LRUCache.java
@@ -1,20 +1,56 @@
-import java.util.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LRUCache {
 
-  Map<Integer, DNode> cache;
-  DNode head;
-  DNode tail;
+  private class DNode {
+    int value;
+    int key;
+    DNode prev, next;
+  }
+
+
+  void add(DNode node) {
+    DNode headNext = head.next;
+
+    node.next = headNext;
+    node.prev = head;
+
+    headNext.prev = node;
+    head.next = node;
+  }
+
+  void remove(DNode node) {
+    DNode pre = node.prev;
+    DNode next = node.next;
+    pre.next = next;
+    next.prev = pre;
+  }
+
+  void moveToHead(DNode node) {
+    this.remove(node);
+    this.add(node);
+  }
+
+  void popTail(DNode node) {
+    DNode tailPrev = tail.prev;
+    remove(tailPrev);
+  }
+
+  Map<Integer, DNode> cache = new HashMap<>();
+  DNode head, tail;
   int capacity;
   int size = 0;
 
   public LRUCache(int capacity) {
     this.capacity = capacity;
     // dummy
-    DNode head = new DNode();
+    head = new DNode();
     head.prev = null;
 
-    DNode tail  = new DNode();
+    tail = new DNode();
     tail.next = null;
 
     head.next = tail;
@@ -34,7 +70,6 @@ public class LRUCache {
   public void put(int key, int value) {
     DNode node = cache.get(key);
 
-
     // add a new key
     if (node == null) {
       DNode newNode = new DNode();
@@ -46,8 +81,8 @@ public class LRUCache {
       size++;
 
       if (size > capacity) {
-        popTail(tail.prev);
         cache.remove(tail.prev.key);
+        popTail(tail.prev);
         size--;
       }
     } else {
@@ -55,52 +90,27 @@ public class LRUCache {
       node.value = value;
       moveToHead(node);
     }
-
-
-  }
-
-  void remove(DNode node) {
-    DNode pre = node.prev;
-    DNode next = node.next;
-    pre.next = next;
-    next.prev = pre;
-  }
-
-  void add(DNode node) {
-    DNode headNext = head.next;
-
-    node.next = headNext;
-    node.prev = head;
-
-    headNext.prev = node;
-    head.next = node;
-  }
-
-  void moveToHead(DNode node) {
-    this.remove(node);
-    this.add(node);
-  }
-
-  void popTail(DNode node) {
-    DNode tailPrev = tail.prev;
-//
-//    tailPrev.prev.next = tail;
-//    tail.prev = tailPrev.prev;
-    remove(tailPrev);
   }
 
 
 
-  private class DNode {
-    int value;
-    int key;
-    DNode prev, next;
 
-//    DNode(int _key, int _value) {
-//      key = _key;
-//      value = _value;
-//    }
 
+
+
+  public static void main(String[] args) {
+    LRUCache lRUCache = new LRUCache(2);
+    lRUCache.put(1, 1); // cache is {1=1}
+    lRUCache.put(2, 2); // cache is {1=1, 2=2}
+    lRUCache.get(1);    // return 1
+    lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+    lRUCache.get(2);    // returns -1 (not found)
+    lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+    lRUCache.get(1);    // return -1 (not found)
+    lRUCache.get(3);    // return 3
+    lRUCache.get(4);    // return 4
+
+    System.out.println(lRUCache);
 
   }
 
