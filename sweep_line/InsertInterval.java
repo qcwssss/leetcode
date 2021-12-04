a0c993133aad3c6ef638d0cdfc4eb85929c16b6c
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-03 16:53:24 -0800

    3 case solution

--- sweep_line/InsertInterval.java
+++ sweep_line/InsertInterval.java
@@ -1,6 +1,6 @@
 public class InsertIntervals {
     // O(n) time
-    public int[][] insert(int[][] intervals, int[] newInterval) {
+    public int[][] insert1(int[][] intervals, int[] newInterval) {
         int N = intervals.length;
         int start = newInterval[0], end = newInterval[1]; //insert
         List<int[]> res = new ArrayList<>();
@@ -22,7 +22,34 @@ public class InsertIntervals {
         }
                 res.add(new int[]{start, end});
 
-        return res.toArray(new int[0][]); // check : list.toArray(T[] a) source code 
+        return res.toArray(new int[0][]); // check : list.toArray(T[] a) source code
+
+    }
+
+    // better approach
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> res = new ArrayList<>();
+        for (int[] cur : intervals) {
+            if ( newInterval == null || newInterval[0] > cur[1]) {
+                // or already added
+                // too early, no overlapping
+                res.add(cur);
+            } else if (newInterval[1] < cur[0]) {
+                // already pass newInterval, and no overlap
+                res.add(newInterval);
+                res.add(cur);
+                newInterval = null; // added
+
+            } else {
+                //overlapping
+                newInterval[0] = Math.min(cur[0], newInterval[0]);
+                newInterval[1] = Math.max(cur[1], newInterval[1]);
+            }
+
+        }
+        if (newInterval != null) res.add(newInterval);
+
+        return res.toArray(new int[0][]);
 
     }
 }
