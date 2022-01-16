268b87981c55a7a852afef1674e41f0b6d2f7e2d
Chen Qiu <qiu.chen1@northeastern.edu>
2022-01-15 18:16:44 -0800

    pass example case

--- amazon/OA1/Keypad.java
+++ amazon/OA1/Keypad.java
@@ -1,43 +1,33 @@
-public class KeyPad {
+import java.util.*;
+
+public class Keypad {
     public static int minPress(String a) {
         Map<Character, Integer> map = new HashMap<>(); // <char, #press>
         char[] arr = a.toCharArray();
         int n = arr.length;
+        List<Character> list = new ArrayList<>();
         for (Character c : arr) {
             map.put(c, map.getOrDefault(c, 0) + 1);
+            if (!list.contains(c))  list.add(c);
         }
-        Collections.sort(arr, (o1, o2) -> (map.get(o2) - map.get(o1)));
-        char prev = arr[0];
-        int buttons = 1;
-        int count = 0;
-        for (int i = 1; i < n; i++) {
-            char cur = arr[i];
-            if (cur != prev) {
-                if (buttons % 9 > 0 && buttons % 9 < 2) {
-                    buttons++;
-                } else if (buttons % 9 > 2) {
-                    buttons += 2;
-                }
-                buttons++;
-                prev = cur;
-            }
-        }
-        return count + n;
+        Collections.sort(list, (o1, o2) -> (map.get(o2) - map.get(o1)));
+
+        int numOfKeys = list.size();
+        int reqTwice = Math.max(0, numOfKeys - 9);
+        int reqThrice = Math.max(0, numOfKeys - 9 * 2);
+        int count = n + reqTwice + reqThrice;
+        return count;
+
     }
 
-    public static int minClickCount(String input) {
-        int times = 0;
-        char[] arr = input.toCharArray();
-        for (char c : arr) {
-            time += (c - 'a') / 9 + 1;
-        }
-        return times;
-    }
 
-    public static void main(String []args) {
-        String a = "abacadefghibj";
+    public static void main(String[] args) {
+            String a = "abacadefghibj";
+// System.out.println(minPress(a));
+        int hello = minPress("hello");
+        int aba = minPress(a);
+        System.out.println(hello);
+        System.out.println(aba);
 
-
-        System.out.println(minPress(a));
     }
 }
